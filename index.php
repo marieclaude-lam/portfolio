9b669829b9c780f26b1f123dac77a4ad5e01835e
marie-claude lam <marieclaude.lam@gmail.com>
2019-11-26 15:45:49 +0100

    header

--- index.php
+++ index.php
@@ -7,7 +7,48 @@
     <title>Marie-Claude LAM</title>
 
 </head>
+<!-----------------------------------------------------------------------------------------------------
+                                            HEADER
+------------------------------------------------------------------------------------------------------->
+    <header>
+    <div>
+    <img src="#" alt="logo">
+    </div>
+    <nav>
+    <div>Qui suis-je?</div>
+    <div>Compétences</div>
+    <div>Parcours</div>
+    <div>Portfolio</div>
+    <div>Contact</div>
+    <div>Blog</div>
+    <div>Log In</div>
+    </nav>
+    <article> 
+        <p>
+            Bienvenue, Je suis
+        </p>
+        <H1>Marie-Claude LAM</H1>
+    </article>
+    <p> Intégratrice Web | Développeuse Front-end & Back-end</p>
+    </header>
+<!-----------------------------------------------------------------------------------------------------
+                                            MAIN
+------------------------------------------------------------------------------------------------------->
+<section>
+    <div>
+        <h2>Qui suis-je</h2>
+    </div>
+    <div>
+    <img src="image/profil1.png" alt="profil">
+    </div>
 
+</section>
+
+<section></section>
+
+<section></section>
+
+<section></section>
 
 <body>
     
