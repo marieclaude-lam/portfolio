5493cc00308d7a9bdfc369d64aa4b033a314b41d
marie-claude lam <marieclaude.lam@gmail.com>
2019-11-28 10:42:12 +0100

    section expérience professionnelle

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="home.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css" integrity="sha384-SI27wrMjH3ZZ89r4o+fGIJtnzkAnFs3E4qz9DIYioCQ5l9Rd/7UAa8DHcaL8jkWt" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.0/js/bootstrap.min.js" integrity="sha384-3qaqj0lc6sV/qpzrc1N5DC6i1VRn/HyX4qdPaiEFbn54VjQBEU341pvjz7Dv3n6P" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/6a5f3d051e.js"></script>
     <title>Marie-Claude LAM</title>
 
 </head>
@@ -23,29 +29,184 @@
     <div>Blog</div>
     <div>Log In</div>
     </nav>
-    <article> 
+    <article>
+    <img src="image/background jesus" alt="" srcset="">
         <p>
             Bienvenue, Je suis
         </p>
         <H1>Marie-Claude LAM</H1>
+    <p>Intégratrice Web | Développeuse Front-end & Back-end</p>
     </article>
-    <p> Intégratrice Web | Développeuse Front-end & Back-end</p>
+
     </header>
 <!-----------------------------------------------------------------------------------------------------
                                             MAIN
 ------------------------------------------------------------------------------------------------------->
+<!-----------------------------------------------------------------------------------------------------
+                                            Qui suis-je
+------------------------------------------------------------------------------------------------------->
+
 <section>
     <div>
         <h2>Qui suis-je</h2>
     </div>
     <div>
-    <img src="image/profil1.png" alt="profil">
+        <img src="image/profil1.jpg" alt="profil" width=10%>
+        <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corporis culpa aliquam aperiam repellendus? Officiis dicta animi unde quas error, iusto molestiae sunt provident libero. Quas, nulla deleniti! Consequatur, aut?</p>
+        </div>
     </div>
-
 </section>
+<!-----------------------------------------------------------------------------------------------------
+                                        Mes Compétences
+------------------------------------------------------------------------------------------------------->
+<section>
+    <div> 
+        <h2>Mes Compétences</h2></div>
+    <div>
+    <article>
+        <div>
+            <h3><i class="fas fa-file-code"></i><br>Développement <br> Front-end</h3>
+        </div>
+        <div>
+            <p>tâches dev front</p>
+            <p>tâches dev front</p>
+            <p>tâches dev front</p>
+        </div>
+        <div>
+            <img src="#" alt="Html5">
+            <img src="#" alt="Javascript">
+            <img src="#" alt="CSS">
+            <img src="#" alt="ReactJS">
+            <img src="#" alt="Canva">
+            <img src="#" alt="BootStrap">
+            <img src="#" alt="SEO">
+        </div>
+    </article>
+    <article>
+        <div>
+            <h3><i class="fas fa-database"></i><br>Développement <br>Back-end</h3>
+        </div>
+        <div>
+            <p>tâches dev back</p>
+            <p>tâches dev back</p>
+            <p>tâches dev back</p>
+        </div>
+        <div>
+            <img src="#" alt="PHP">
+            <img src="#" alt="SQL">
+            <img src="#" alt="POO">
+        </div>
+    </article>
+    <article>
+        <div>
+            <h3><i class="fas fa-globe"></i><br></i>et plus encore</h3>
+        </div>
+        <div>
+            <p>tâches dev front</p>
+            <p>tâches dev front</p>
+            <p>tâches dev front</p>
+        </div>
+        <div>
+            <img src="#" alt="Wordpress">
+            <img src="#" alt="Jimdo">
+            <img src="#" alt="Wix">
+            <img src="#" alt="Adobe Photoshop">
+            <img src="#" alt="Magento">
+            <img src="#" alt="SAP">
+            <img src="#" alt="Jquery">
+            <img src="#" alt="Wix">
+        </div>
+    </article>
+</section>
+<!-----------------------------------------------------------------------------------------------------
+                                           Expérience Pro
+------------------------------------------------------------------------------------------------------->
+<section id="experience">
+    <div class="container">
+        <div class="white-divider"></div>
+        <div class="heading">
+            <h2>Experience Professionnelle</h2>
+        </div>
+        <ul class="timeline">
+            <li>
+                <div class="timeline-badge"><i class="fas fa-briefcase"></i>
+                </div>
+                <div class="timeline-panel-container">
+                    <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h3>FANVOICE</h3>
+                                <h4>Stage développeur Web</h4>
+                                <p class="text-muted"><small><i class="far fa-clock"></i> 2019 </small></p>
+                            </div>
+                            <div class="timeline-body">
+                            <p>Test de code en PHP, Javascript.</p>
+                            <p>Vérification de la syntaxe en HTML, CSS.</p>
+                            </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-badge"><i class="fas fa-briefcase"></i>
+                </div>
+                <div class="timeline-panel-container-inverted">
+                    <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h3>SOCIETE FRANÇAISE DES MONNAIES PARIS 19</h3>
+                                <h4>Assistante du Responsable Offre Digitale e-commerce</h4>
+                                <p class="text-muted"><small><i class="far fa-clock"></i> 2017-2019 </small></p>
+                            </div>
+                            <div class="timeline-body">
+                            <p>Création des fiches produits sur le site d’e-commerce. (Magento)</p>
+                            <p>Sélection et adaptation des photos. (Photoshop, Scene7)</p>
+                            <p>Veille réseaux sociaux. (Facebook)</p>
+                            </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-badge"><i class="fas fa-briefcase"></i>
+                </div>
+                <div class="timeline-panel-container">
+                    <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h3>SOCIETE FRANÇAISE DES MONNAIES PARIS 19</h3>
+                                <h4>Conseillère Clientèle</h4>
+                                <p class="text-muted"><small><i class="far fa-clock"></i> 2012-2017 </small></p>
+                            </div>
+                            <div class="timeline-body">
+                            <p>Saisie de commande clients, vérifications des écarts de saisie.(SAP)</p>
+                            <p>Suivi des comptes clients : remboursement, retours commandes, fidélisation, relances.</p>
+                            <p>Service après-vente : adapter une solution à un litige client</p>
+                            <p>ventes additionnelles par téléphone.</p>
+                            </div>
+                    </div>
+                </div>
+            </li>
+            <li>
+                <div class="timeline-badge"><i class="fas fa-briefcase"></i>
+                </div>
+                <div class="timeline-panel-container-inverted">
+                    <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                <h3>BOUYGUES CONSTRUCTION Chilly Mazarin</h3>
+                                <h4>Controleur de gestion</h4>
+                                <p class="text-muted"><small><i class="far fa-clock"></i> 2011 </small></p>
+                            </div>
+                            <div class="timeline-body">
+                            <p>Examen des écarts de facturations liés aux achats et locations des matériaux (grues, containers, ...)</p>
+                            <p>(SAP, Hermès logiciel interne)</p>
+                            </div>
+                    </div>
+                </div>
+            </li>
 
-<section></section>
-
+        </ul>
+    </div>
+</section>
+<!-----------------------------------------------------------------------------------------------------
+                                           Formations
+------------------------------------------------------------------------------------------------------->
 <section></section>
 
 <section></section>
